d70c049abf96bedb2c0566806af35502fafd76fd
Fedor Shamakhov <fedor.shamakhov@gmail.com>
2020-12-10 20:09:15 +0800

    📊 Initial bunch of graphs

---
+++ scipts/sprites.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+java -jar plantuml.jar -encodesprite 4 C4person100.png > C4person4.sprite
+java -jar plantuml.jar -encodesprite 8 C4person100.png > C4person8.sprite
+java -jar plantuml.jar -encodesprite 16 C4person100.png > C4person16.sprite
